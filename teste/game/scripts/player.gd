3e741a847960e06643e8f5fd7be3afba94620161
VCB2000 <2000barrosvic@gmail.com>
2025-01-28 16:45:12 -0300

    Primeiro Commit oficial

---
+++ teste/game/scripts/player.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+@onready var _animation_player = $Anim
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -500.0
+var isJump = false
+
+
+func _physics_process(delta: float) -> void:
+	if is_on_floor():
+		isJump = false
+
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+
+	# Handle jump.
+	if Input.is_action_just_pressed("tecla_w") and is_on_floor():
+		isJump = true
+		velocity.y = JUMP_VELOCITY
+		_animation_player.play("jump")
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("tecla_a", "tecla_d")
+	if direction:
+		velocity.x = direction * SPEED
+		if is_on_floor() and not isJump:
+			_animation_player.play("walk")
+	elif is_on_floor:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor() and not isJump:
+			_animation_player.play("idle")
+	if direction < 0: 
+		_animation_player.flip_h = true
+	if direction > 0: 
+		_animation_player.flip_h = false
+	move_and_slide()
